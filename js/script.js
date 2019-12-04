356bf83deb13372039de68c5af8b9ecaeaf0cb23
Benjamin Forg <b.l.forgd.c.forg@gmail.com>
2019-12-04 14:08:41 +1030

    Added some changes to reuse the same classes for another sprite image

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-async function playanimation() {
+async function play_ciyf_animation() {
   var element = document.getElementById("ciyf-sprite");
   element.classList.add("animate-ciyf");
   await sleep(6000);
